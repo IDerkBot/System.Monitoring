d6e1e1d8f5dc42c2490ab82345a3c47368fef3de
IDerkBot <ivanderk2003@gmail.com>
2023-05-13 12:56:24 +0300

    Загрузил файлы которые не были добавлены в GIT

---
+++ Monitoring.Models/DB.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Monitoring.Models.Entity;
+
+namespace Monitoring.Models;
+
+/// <summary>
+/// 
+/// </summary>
+public static class Db
+{
+    public static string? DistrictName { get; set; }
+    public static double SizeWindow { get; set; }
+    public static string Path { get; set; }
+    public static string ConfigName { get; set; } = "settings";
+    public static string ConfigFormat { get; set; } = "json";
+    public static string DirectoryName { get; set; } = "SystemMonitoring";
+    public static Seed SelectSeeding { get; set; }
+}
---
+++ Monitoring.Models/Validation.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Monitoring.Models
+{
+    public static class Validation
+    {
+        public static bool IsEmail(string email)
+        {
+            var regex = new Regex("([a-z]|[0-9]){3,}@(gmail.com|yandex.ru|mail.com)");
+            return regex.IsMatch(email);
+        }
+
+
+        public static bool IsPhone(string phone)
+        {
+            var regex = new Regex("^(\\+7|7|8)9[0-9][0-9]( |)(\\(|)[0-9][0-9][0-9](\\)|\\) | |)[0-9][0-9](-|)[0-9][0-9]");
+            return regex.IsMatch(phone);
+        }
+    }
+}
