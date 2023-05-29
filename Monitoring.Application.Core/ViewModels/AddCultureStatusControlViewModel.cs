cd940d5624106d8a45935457ac61e08ff3ceadc4
IDerkBot <ivanderk2003@gmail.com>
2023-05-29 21:57:23 +0300

    Добавлены модель-представления для представлений культур

---
+++ Monitoring.Application.Core/ViewModels/AddCultureStatusControlViewModel.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Monitoring.Models.Entity;
+using SystemMonitoringNetCore.Infrastructure.Command;
+using SystemMonitoringNetCore.Models;
+using SystemMonitoringNetCore.ViewModels.Base;
+
+namespace SystemMonitoringNetCore.ViewModels;
+
+public class AddCultureStatusControlViewModel : BaseViewModel
+{
+    #region Properties
+
+    #region CurrentCultureStatus : CultureStatus - Статус культуры
+
+    private CultureStatus _currentCultureStatus;
+
+    /// <summary> Статус культуры </summary>
+    public CultureStatus CurrentCultureStatus
+    {
+        get => _currentCultureStatus;
+        set => SetField(ref _currentCultureStatus, value);
+    }
+
+    #endregion CurrentCultureStatus
+
+    #region ListCultures : ObservableCollection<Culture> - Список культур
+
+    private ObservableCollection<Culture> _listCultures;
+
+    /// <summary> Список культур </summary>
+    public ObservableCollection<Culture> ListCultures
+    {
+        get => _listCultures;
+        set => SetField(ref _listCultures, value);
+    }
+
+    #endregion ListCultures
+    
+    #endregion
+
+    #region Commands
+
+    #region Loaded - Загрузка
+
+    /// <summary> Загрузка </summary>
+    public ICommand LoadedCommand { get; }
+
+    private void OnLoadedCommandExecuted(object parameter)
+    {
+        ListCultures.Clear();
+        foreach (var culture in Db.DbContext.Cultures)
+            ListCultures.Add(culture);
+    }
+
+    #endregion Loaded
+
+    #region Save - Сохранение данных
+
+    /// <summary> Сохранение данных </summary>
+    public ICommand SaveCommand { get; }
+
+    private void OnSaveCommandExecuted(object parameter)
+    {
+        if (CurrentCultureStatus.Id == 0) Db.DbContext.CultureStatuses.Add(CurrentCultureStatus);
+        Db.DbContext.SaveChanges();
+        ManagerPage.Back();
+    }
+
+    private bool CanSaveCommandExecute(object parameter) =>
+        !string.IsNullOrWhiteSpace(CurrentCultureStatus.Status) &&
+        CurrentCultureStatus.StartPeriod > 0 &&
+        CurrentCultureStatus.EndPeriod > 0 &&
+        CurrentCultureStatus.StartingValueCalcium > 0 &&
+        CurrentCultureStatus.EndingValueCalcium > 0 &&
+        CurrentCultureStatus.StartingValueTemperature > 0 &&
+        CurrentCultureStatus.EndingValueTemperature > 0 &&
+        CurrentCultureStatus.StartingValueHumidity > 0 &&
+        CurrentCultureStatus.EndingValueHumidity > 0 &&
+        CurrentCultureStatus.StartingValueMagnesium > 0 &&
+        CurrentCultureStatus.EndingValueMagnesium > 0 &&
+        CurrentCultureStatus.StartingValueNitrogen > 0 &&
+        CurrentCultureStatus.EndingValueNitrogen > 0 &&
+        CurrentCultureStatus.StartingValuePh > 0 &&
+        CurrentCultureStatus.EndingValuePh > 0 &&
+        CurrentCultureStatus.StartingValuePhosphor > 0 &&
+        CurrentCultureStatus.EndingValuePhosphor > 0 &&
+        CurrentCultureStatus.StartingValuePotassium > 0 &&
+        CurrentCultureStatus.EndingValuePotassium > 0;
+    
+
+    #endregion Save
+
+    #region AddNewCulture - Добавление новой культуры
+
+    /// <summary> Добавление новой культуры </summary>
+    public ICommand AddNewCultureCommand { get; }
+
+    private void OnAddNewCultureCommandExecuted(object parameter)
+    {
+        ManagerPage.Navigate(new AddCultureControlViewModel());
+    }
+
+    #endregion AddNewCulture
+    
+    #endregion
+
+    #region Contructor
+
+    public AddCultureStatusControlViewModel(CultureStatus? cultureStatus = null)
+    {
+        CurrentCultureStatus = cultureStatus ?? new CultureStatus();
+        LoadedCommand = new RelayCommand(OnLoadedCommandExecuted);
+        SaveCommand = new RelayCommand(OnSaveCommandExecuted, CanSaveCommandExecute);
+        AddNewCultureCommand = new RelayCommand(OnAddNewCultureCommandExecuted);
+        ListCultures = new ObservableCollection<Culture>();
+    }
+
+    #endregion
+}
---
+++ Monitoring.Application.Core/ViewModels/CulturesControlViewModel.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
+using Monitoring.Models.Entity;
+using SystemMonitoringNetCore.Infrastructure.Command;
+using SystemMonitoringNetCore.Models;
+using SystemMonitoringNetCore.ViewModels.Base;
+using SystemMonitoringNetCore.Views.UserControls;
+
+namespace SystemMonitoringNetCore.ViewModels;
+
+public class CulturesControlViewModel : BaseViewModel
+{
+    #region Properties
+
+    #region Cultures : ObservableCollection<CultureStatus> - Список культур
+
+    private ObservableCollection<CultureStatus> _cultures;
+
+    /// <summary> Список культур </summary>
+    public ObservableCollection<CultureStatus> Cultures
+    {
+        get => _cultures;
+        set => SetField(ref _cultures, value);
+    }
+
+    #endregion Cultures
+
+    #region SelectedCulture : CultureStatus - Выбранный статус культуры
+
+    private CultureStatus _selectedCultureStatus;
+
+    /// <summary> Выбранный статус культуры </summary>
+    public CultureStatus SelectedCultureStatus
+    {
+        get => _selectedCultureStatus;
+        set => SetField(ref _selectedCultureStatus, value);
+    }
+
+    #endregion SelectedCulture
+
+    #endregion
+
+    #region Loaded - Загрузка окна
+
+    /// <summary> Загрузка окна </summary>
+    public ICommand LoadedCommand { get; }
+
+    private void OnLoadedCommandExecuted(object parameter)
+    {
+        UpdateCollection();
+    }
+
+    #endregion Loaded
+
+    #region Add - Добавляет новый статус культуры
+
+    /// <summary> Добавляет новый статус культуры </summary>
+    public ICommand AddCommand { get; }
+
+    private void OnAddCommandExecuted(object parameter) => ManagerPage.Navigate(new AddCultureStatusControlViewModel());
+
+    #endregion Add
+
+    #region Delete - Удаление статуса культуры
+
+    /// <summary> Удаление статуса культуры </summary>
+    public ICommand DeleteCommand { get; }
+
+    private void OnDeleteCommandExecuted(object parameter)
+    {
+        if (parameter is CultureStatus cultureStatus)
+        {
+            Db.DbContext.CultureStatuses.Remove(cultureStatus);
+            Db.DbContext.SaveChanges();
+            
+            UpdateCollection();
+        }
+    }
+
+    private bool CanDeleteCommandExecute(object parameter) => parameter is CultureStatus;
+
+    #endregion Delete
+
+    #region Edit - Редактирование статуса культуры
+
+    /// <summary> Редактирование статуса культуры </summary>
+    public ICommand EditCommand { get; }
+
+    private void OnEditCommandExecuted(object parameter)
+    {
+        if (parameter is CultureStatus cultureStatus)
+        {
+            var vm = new AddCultureStatusControlViewModel(cultureStatus);
+            ManagerPage.Navigate(new AddCultureStatusControl { DataContext = vm });
+        } 
+        
+    }
+
+    private bool CanEditCommandExecute(object parameter) => parameter is CultureStatus;
+
+    #endregion Edit
+
+    #region Constructor
+
+    public CulturesControlViewModel()
+    {
+        LoadedCommand = new RelayCommand(OnLoadedCommandExecuted);
+        AddCommand = new RelayCommand(OnAddCommandExecuted);
+        DeleteCommand = new RelayCommand(OnDeleteCommandExecuted, CanDeleteCommandExecute);
+        EditCommand = new RelayCommand(OnEditCommandExecuted, CanEditCommandExecute);
+        
+        Cultures = new ObservableCollection<CultureStatus>();
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    private void UpdateCollection()
+    {
+        Cultures.Clear();
+        foreach (var culture in Db.DbContext.CultureStatuses.ToList()) 
+            Cultures.Add(culture);
+    }
+
+    #endregion
+}
