8f3af4278496679e87348807b34349bc93f5733b
IDerkBot <ivanderk2003@gmail.com>
2023-05-29 21:56:58 +0300

    Добавлены Представления для культур

---
+++ Monitoring.Application.Core/Views/UserControls/AddCultureControl.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace SystemMonitoringNetCore.Views.UserControls;
+
+public partial class AddCultureControl : UserControl
+{
+    public AddCultureControl()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Monitoring.Application.Core/Views/UserControls/AddCultureStatusControl.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace SystemMonitoringNetCore.Views.UserControls;
+
+public partial class AddCultureStatusControl : UserControl
+{
+    public AddCultureStatusControl()
+    {
+        InitializeComponent();
+    }
+}
